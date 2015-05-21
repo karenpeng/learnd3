5359bde8165c1dd71b79f60ffa0d8b21139cdd20
Karen <karenpenglabs@gmail.com>
2015-05-21 17:17:19 -0400

    lol so fun

--- sketch5.js
+++ sketch5.js
@@ -13,35 +13,58 @@ function update(_data){
   var text = svg.selectAll('text')
   .data(_data)
 
+  //重排d和i
+  //{
+  //  i : index,
+  //  d : data
+  //}
+
   //exit
+  //去除多余元素
   text.exit().remove()
 
   //update
+  //按照i把d与元素绑定
   text
   .attr('class', 'update')
+   .text(function(d, i){
+    console.log('update')
+    console.log(d, i)
+    return d;
+  })
 
   //enter
+  //剩下的i把d与元素绑定
   text
   .enter().append('text')
   .attr('class', 'enter')
   .attr('x', function(d, i){return i *  30})
   .attr('dy', '.35em')
+   .text(function(d, i){
+    console.log('enter')
+    console.log(d, i)
+    return d;
+  })
 
 
   //enter + update???
-  text
-  .text(function(d){return d;})
+  // text
+  // .text(function(d){return d;})
 
 }
 
 update(alphabet)
 
-setInterval(function(){
+function renew(){
   var ran = Math.round(Math.random()*26);
   var arr = alphabet.slice(Math.min(ran, 26 - ran), Math.max(ran, 26 - ran));
-  console.log(ran, arr)
+  //console.log(ran, arr)
+  console.log('---------------------')
   update(arr)
-}, 1000)
+}
+
+d3.select('svg')
+.on('click', renew)
 
 
 // Shuffles the input array.
--- sketch6.js
+++ sketch6.js
@@ -12,47 +12,83 @@ var svg = d3.select("body").append("svg")
 function update(_data){
   var text = svg.selectAll('text')
   .data(_data, function(d, i){
+    // console.log('update')
+    // console.log(d, i)
+    // 绑定d和元素
+    return d
+  })
+  //.data(_data)
+  
+  //重排i
+  //{ 
+  //  i : index,
+  //  d : data
+  //}
+  //i gets recompute here
+  //what's left is to assign to element
+
+
+  //update
+  //显示？已有元素的d
+  text
+  .attr('class', 'update')
+  .text(function(d, i){
     console.log('update')
     console.log(d, i)
     return d
   })
-  //.data(_data)
-
-  //exit
-  text.exit().remove()
-
-  //update
-  text
-  .attr('class', 'update')
-  .text(function(d){return d})
+  .transition()
+  .attr('x', function(d, i){
+    return i * 32
+  })
+ 
 
   //enter
+  //把没有绑定的d绑定到新的元素上
   text
   .enter().append('text')
   .attr('class', 'enter')
+  .attr('dy', '.35em')
+  .attr('y', -60)
+  .style('opacity', 0)
+  .transition()
+  .attr('y', 0)
   .attr('x', function(d, i){
     console.log('enter')
     console.log(d , i)
-    return i *  30
+    return i *  32
   })
-  .attr('dy', '.35em')
+  .style('opacity', 1)
   .text(function(d){
     return d
   })
+
+  //exit
+  //去除多余元素
+  text.exit()
+  .transition()
+  .attr('y', 60)
+  .style('fill', function(d, i){
+    console.log('exit')
+    console.log(d, i)  
+    return 'red'
+  })
+  .style('fill-opacity', 0)
+  .remove()
 
 /*
   //enter + update???
   text
   .text(function(d){return d;})
 */
-
   //enter + update
-  text
-  .attr('x', function(d, i){
-    console.log('recompute')
-    console.log(d, i)
-    return i * 30;
-  })
+  // text
+  // .attr('x', function(d, i){
+  //   // console.log('recompute')
+  //   // console.log(d, i)
+  //   return i * 30;
+  // })
+
 }
 
 update(alphabet)
@@ -63,11 +99,19 @@ update(alphabet)
 //   //console.log(ran, arr)
 //   update(arr)
 // }, 1000)
+var data = [
+  ['b', 'd', 'e'],
+  ['a', 'b', 'f', 'g']
+]
+i = 0
 function renew(){
   console.log('------------------------')
-  var ran = Math.round(Math.random()*26);
-  var arr = alphabet.slice(Math.min(ran, 26 - ran), Math.max(ran, 26 - ran));
-  //console.log(ran, arr)
+  // var ran = Math.round(Math.random()*26);
+  // var arr = alphabet.slice(Math.min(ran, 26 - ran), Math.max(ran, 26 - ran));
+  // //console.log(ran, arr)
+  // update(arr)
+  var arr = shuffle(alphabet).slice(0, Math.floor(Math.random()*26)).sort()
+  //update(data[i++])
   update(arr)
 }
 

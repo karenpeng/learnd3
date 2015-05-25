d9a14af0aaad7ccf04a594087252a8c698a67366
Karen <karenpenglabs@gmail.com>
2015-05-25 19:44:44 -0400

    why

--- sketch9.js
+++ sketch9.js
@@ -14,42 +14,130 @@ var data = [
 ]
 
 var datas = [
-  
+  [
+    {
+      key: 'karen',
+      value : 2
+    },
+    {
+      key: 'john',
+      value: 8
+    },
+    {
+      key: 'ben',
+      value: 4
+    }
+  ],
+  [
+    {
+      key: 'mike',
+      value: 5
+    },
+    {
+      key: 'karen',
+      value : 2
+    },
+    {
+      key: 'kelly',
+      value: 6
+    },
+    {
+      key: 'ben',
+      value: 7
+    }
+  ],
+  [
+    {
+      key: 'mike',
+      value: 5
+    },
+    {
+      key: 'jane',
+      value: 6
+    }
+  ]
 ]
 
+var width = 10000
+var height = 300
 var svg = d3.select('body').append('svg')
-var barWidth = 40;
-var barHeightScala = 20;
+.attr('width', 10000)
+.attr('height', height)
 
-var bar = svg.selectAll('g')
-.data(data)
-.enter()
-.append('g')
-.attr('transform', function(d, i){
-  return 'translate(' + i * barWidth + ', 0)';})
+var barWidth = 40
 
-var barHeight = d3.max(data, function(d){
-  return d.value
+var barHeightScala = d3.scale.linear()
+.range([0, height])
+
+function update(data){
+
+  barHeightScala.domain([0, d3.max(data, function(d){
+    return d.value
+  })])
+
+  var barHeight = d3.max(data, function(d){
+    return d.value
+  })
+
+  var bar = svg.selectAll('g')
+    .data(data, function(d){
+      console.log(d)
+      return d.key
+    })
+
+  // bar
+  // .attr('transform', function(d, i){
+  //   return 'translate(' + i * barWidth + ', 0)' 
+  // })
+
+  bar.selectAll('rect')
+  .style('fill', 'black')
+
+  var barEnter = bar
+  .enter()
+  .append('g')
+
+  barEnter
+  .append('rect')
+  .style('fill', 'blue')
+
+  barEnter
+  .append('text') 
+
+  bar
+  .attr('transform', function(d, i){
+    return 'translate(' + i * barWidth + ', 0)' 
+  })
+
+  var rects = d3.selectAll('rect')
+    .attr('height', function(d){
+      return barHeightScala(d.value)
+    })
+    .attr('width', barWidth - 1)
+    .attr('y', function(d){
+      return barHeightScala(barHeight - d.value)
+    })
+
+  var texts = d3.selectAll('text')
+    .style('fill', 'white')
+    .style('font-size', '10px')
+    .attr('y', barHeightScala(barHeight))
+    .attr('dx', '0.4em')
+    .attr('dy', '-2em')
+    .text(function(d){
+      return d.key
+    })
+
+  bar
+  .exit()
+  .remove()
+}
+
+var index = 0
+d3.select('svg').on('click', function(){
+  console.log('---------------')
+  update(datas[index])
+  index++
+  if(index > 3) index = 0
 })
 
-bar
-.append('rect')
-.attr('height', function(d){
-  return d.value * barHeightScala
-})
-.attr('width', barWidth - 1)
-.attr('y', function(d){
-  return (barHeight - d.value) * barHeightScala;
-})
-.style('fill', 'green')
-
-bar
-.append('text')
-.style('fill', 'white')
-.style('font-size', '10px')
-.attr('y', barHeight * barHeightScala)
-.attr('dx', '0.4em')
-.attr('dy', '-2em')
-.text(function(d){
-  return d.key
-})
